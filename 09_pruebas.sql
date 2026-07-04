058be4123ce9eb8d816568760027e050af7ebcd6
Gabriel Gonzalez <gabriel.gonzalez20@utp.ac.pa>
2026-07-04 13:16:42 -0500

    Actualiza pruebas principales

--- 09_pruebas.sql
+++ 09_pruebas.sql
@@ -4,10 +4,7 @@
 
 SET SERVEROUTPUT ON;
 
---===============================================
--- 1. PRUEBA CARGA DE TABLAS PARAMETRICAS
---===============================================
-
+-- 1. CARGA PARAMETRICA
 BEGIN
     sp_cargar_parametricas(
         'PSICOLOGO CLINICO',
@@ -29,11 +26,7 @@ SELECT * FROM TipoTlf_Est;
 SELECT * FROM TipoTlf_Docente;
 SELECT * FROM TipoTlf_Admin;
 
-
---===============================================
--- 2. PRUEBA INSERTAR PSICOLOGO
---===============================================
-
+-- 2. INSERTAR PSICOLOGO CON TELEFONO
 DECLARE
     v_id_cargo cargo.id_cargo%TYPE;
 BEGIN
@@ -50,6 +43,7 @@ BEGIN
         'P-TEST-001',
         'FEMENINO',
         'ana@utp.ac.pa',
+        60000000,
         'CASA 1',
         'CALLE 1',
         'BELLA VISTA',
@@ -61,11 +55,7 @@ END;
 SELECT * FROM Psicologo
 WHERE cedula = 'P-TEST-001';
 
-
---===============================================
--- 3. PRUEBA INSERTAR ESTUDIANTE ATENDIDO
---===============================================
-
+-- 3. INSERTAR ESTUDIANTE
 DECLARE
     v_id_carrera Carrera.id_carrera%TYPE;
     v_id_tipo TipoTlf_Est.id_tipo%TYPE;
@@ -100,14 +90,9 @@ END;
 
 SELECT * FROM Estudiante
 WHERE cedula = 'E-TEST-001';
-
 SELECT * FROM Tlf_Estudiante;
 
-
---===============================================
--- 4. PRUEBA INSERTAR DOCENTE ATENDIDO
---===============================================
-
+-- 4. INSERTAR DOCENTE
 DECLARE
     v_id_facultad Facultad.id_facultad%TYPE;
     v_id_tipo TipoTlf_Docente.id_tipo%TYPE;
@@ -143,14 +128,9 @@ END;
 
 SELECT * FROM Docente
 WHERE cedula = 'D-TEST-001';
-
 SELECT * FROM Tlf_Docente;
 
-
---===============================================
--- 5. PRUEBA INSERTAR ADMINISTRATIVO ATENDIDO
---===============================================
-
+-- 5. INSERTAR ADMINISTRATIVO
 DECLARE
     v_id_tipo TipoTlf_Admin.id_tipo%TYPE;
 BEGIN
@@ -180,72 +160,24 @@ END;
 
 SELECT * FROM Administrativo
 WHERE cedula = 'A-TEST-001';
-
 SELECT * FROM Tlf_Admin;
 
-
---===============================================
--- 6. PRUEBA FUNCION TIPO DE PACIENTE
---===============================================
-
-SELECT 
-    fn_tipo_paciente_cita(
-        (SELECT id_paciente FROM Estudiante WHERE cedula = 'E-TEST-001'),
-        NULL,
-        NULL
-    ) AS tipo_paciente_estudiante
+-- 6. FUNCIONES DE PACIENTE
+SELECT fn_tipo_paciente_cita(
+    (SELECT id_paciente FROM Estudiante WHERE cedula = 'E-TEST-001'),
+    NULL,
+    NULL
+) AS tipo_paciente_estudiante
 FROM dual;
 
-SELECT 
-    fn_tipo_paciente_cita(
-        NULL,
-        (SELECT id_paciente FROM Docente WHERE cedula = 'D-TEST-001'),
-        NULL
-    ) AS tipo_paciente_docente
+SELECT fn_nombre_paciente_cita(
+    (SELECT id_paciente FROM Estudiante WHERE cedula = 'E-TEST-001'),
+    NULL,
+    NULL
+) AS nombre_estudiante
 FROM dual;
 
-SELECT 
-    fn_tipo_paciente_cita(
-        NULL,
-        NULL,
-        (SELECT id_paciente FROM Administrativo WHERE cedula = 'A-TEST-001')
-    ) AS tipo_paciente_admin
-FROM dual;
-
-
---===============================================
--- 7. PRUEBA FUNCION NOMBRE PACIENTE DE CITA
---===============================================
-
-SELECT 
-    fn_nombre_paciente_cita(
-        (SELECT id_paciente FROM Estudiante WHERE cedula = 'E-TEST-001'),
-        NULL,
-        NULL
-    ) AS nombre_estudiante
-FROM dual;
-
-SELECT 
-    fn_nombre_paciente_cita(
-        NULL,
-        (SELECT id_paciente FROM Docente WHERE cedula = 'D-TEST-001'),
-        NULL
-    ) AS nombre_docente
-FROM dual;
-
-SELECT 
-    fn_nombre_paciente_cita(
-        NULL,
-        NULL,
-        (SELECT id_paciente FROM Administrativo WHERE cedula = 'A-TEST-001')
-    ) AS nombre_administrativo
-FROM dual;
-
-
---===============================================
--- 8. PRUEBA INSERTAR CITA PARA ESTUDIANTE
---===============================================
-
+-- 7. INSERTAR CITA CON HORA DATE
 DECLARE
     v_id_servicio Servicio.id_servicio%TYPE;
     v_id_psicologo Psicologo.id_psico%TYPE;
@@ -268,7 +200,7 @@ BEGIN
 
     sp_insertar_cita(
         TO_DATE('2026-07-15', 'YYYY-MM-DD'),
-        '09:00',
+        TO_DATE('09:00', 'HH24:MI'),
         v_id_servicio,
         v_id_estudiante,
         NULL,
@@ -279,108 +211,10 @@ END;
 /
 
 SELECT * FROM Cita;
+SELECT * FROM auditoria_cita ORDER BY aud_id_auditoria;
+SELECT * FROM estadistica_atenciones ORDER BY id_psicologo, id_servicio, tipo_paciente;
 
-
---===============================================
--- 9. PRUEBA INSERTAR CITA PARA DOCENTE
---===============================================
-
-DECLARE
-    v_id_servicio Servicio.id_servicio%TYPE;
-    v_id_psicologo Psicologo.id_psico%TYPE;
-    v_id_docente Docente.id_paciente%TYPE;
-BEGIN
-    SELECT id_servicio
-    INTO v_id_servicio
-    FROM Servicio
-    WHERE UPPER(nombre_servicio) = 'ORIENTACION';
-
-    SELECT id_psico
-    INTO v_id_psicologo
-    FROM Psicologo
-    WHERE cedula = 'P-TEST-001';
-
-    SELECT id_paciente
-    INTO v_id_docente
-    FROM Docente
-    WHERE cedula = 'D-TEST-001';
-
-    sp_insertar_cita(
-        TO_DATE('2026-07-16', 'YYYY-MM-DD'),
-        '10:00',
-        v_id_servicio,
-        NULL,
-        v_id_docente,
-        NULL,
-        v_id_psicologo
-    );
-END;
-/
-
-SELECT * FROM Cita;
-
-
---===============================================
--- 10. PRUEBA INSERTAR CITA PARA ADMINISTRATIVO
---===============================================
-
-DECLARE
-    v_id_servicio Servicio.id_servicio%TYPE;
-    v_id_psicologo Psicologo.id_psico%TYPE;
-    v_id_admin Administrativo.id_paciente%TYPE;
-BEGIN
-    SELECT id_servicio
-    INTO v_id_servicio
-    FROM Servicio
-    WHERE UPPER(nombre_servicio) = 'ORIENTACION';
-
-    SELECT id_psico
-    INTO v_id_psicologo
-    FROM Psicologo
-    WHERE cedula = 'P-TEST-001';
-
-    SELECT id_paciente
-    INTO v_id_admin
-    FROM Administrativo
-    WHERE cedula = 'A-TEST-001';
-
-    sp_insertar_cita(
-        TO_DATE('2026-07-17', 'YYYY-MM-DD'),
-        '11:00',
-        v_id_servicio,
-        NULL,
-        NULL,
-        v_id_admin,
-        v_id_psicologo
-    );
-END;
-/
-
-SELECT * FROM Cita;
-
-
---===============================================
--- 11. VERIFICAR TRIGGER DE AUDITORIA DE CITAS
---===============================================
-
-SELECT * 
-FROM auditoria_cita
-ORDER BY aud_id_auditoria;
-
-
---===============================================
--- 12. VERIFICAR TRIGGER DE ESTADISTICA DE ATENCIONES
---===============================================
-
-SELECT *
-FROM estadistica_atenciones
-ORDER BY id_psicologo, id_servicio, tipo_paciente;
-
-
---===============================================
--- 13. PRUEBA REPROGRAMAR CITA
---===============================================
-
+-- 8. REPROGRAMAR CITA CON HORA DATE
 DECLARE
     v_id_cita Cita.id_cita%TYPE;
     v_id_servicio Servicio.id_servicio%TYPE;
@@ -390,8 +224,8 @@ BEGIN
     INTO v_id_cita
     FROM Cita
     WHERE id_estudiante = (
-        SELECT id_paciente 
-        FROM Estudiante 
+        SELECT id_paciente
+        FROM Estudiante
         WHERE cedula = 'E-TEST-001'
     )
     AND ROWNUM = 1;
@@ -409,7 +243,7 @@ BEGIN
     sp_reprogramar_cita(
         v_id_cita,
         TO_DATE('2026-07-20', 'YYYY-MM-DD'),
-        '13:30',
+        TO_DATE('13:30', 'HH24:MI'),
         v_id_servicio,
         v_id_psicologo
     );
@@ -418,29 +252,17 @@ END;
 
 SELECT * FROM Cita;
 
-SELECT * 
-FROM auditoria_cita
-ORDER BY aud_id_auditoria;
-
-SELECT *
-FROM estadistica_atenciones
-ORDER BY id_psicologo, id_servicio, tipo_paciente;
-
-
---===============================================
--- 14. PRUEBA ELIMINACION LOGICA DE CITA
---===============================================
-
+-- 9. MARCAR CITA COMO ELIMINADA LOGICAMENTE
 DECLARE
     v_id_cita Cita.id_cita%TYPE;
 BEGIN
     SELECT id_cita
     INTO v_id_cita
     FROM Cita
-    WHERE id_docente = (
-        SELECT id_paciente 
-        FROM Docente 
-        WHERE cedula = 'D-TEST-001'
+    WHERE id_estudiante = (
+        SELECT id_paciente
+        FROM Estudiante
+        WHERE cedula = 'E-TEST-001'
     )
     AND ROWNUM = 1;
 
@@ -448,167 +270,11 @@ BEGIN
 END;
 /
 
-SELECT * FROM Cita;
+SELECT * FROM Cita WHERE estado = 'ELIMINADA';
+SELECT * FROM vw_expediente_paciente;
+SELECT * FROM vw_rep_atenciones_psico;
 
-SELECT * 
-FROM Cita
-WHERE estado = 'ELIMINADA';
-
-SELECT * 
-FROM auditoria_cita
-ORDER BY aud_id_auditoria;
-
-SELECT *
-FROM estadistica_atenciones
-ORDER BY id_psicologo, id_servicio, tipo_paciente;
-
-
---===============================================
--- 15. PRUEBA DIRECTA DEL TRIGGER PACIENTE UNICO
--- DEBE GENERAR ERROR CONTROLADO
---===============================================
-
-DECLARE
-    v_id_servicio Servicio.id_servicio%TYPE;
-    v_id_psicologo Psicologo.id_psico%TYPE;
-    v_id_estudiante Estudiante.id_paciente%TYPE;
-    v_id_docente Docente.id_paciente%TYPE;
-BEGIN
-    SELECT id_servicio
-    INTO v_id_servicio
-    FROM Servicio
-    WHERE UPPER(nombre_servicio) = 'ORIENTACION';
-
-    SELECT id_psico
-    INTO v_id_psicologo
-    FROM Psicologo
-    WHERE cedula = 'P-TEST-001';
-
-    SELECT id_paciente
-    INTO v_id_estudiante
-    FROM Estudiante
-    WHERE cedula = 'E-TEST-001';
-
-    SELECT id_paciente
-    INTO v_id_docente
-    FROM Docente
-    WHERE cedula = 'D-TEST-001';
-
-    INSERT INTO Cita(
-        id_cita,
-        fecha,
-        hora,
-        id_servicio,
-        id_estudiante,
-        id_docente,
-        id_admin,
-        id_psicologo,
-        estado
-    ) VALUES (
-        seq_id_cita.NEXTVAL,
-        TO_DATE('2026-07-21', 'YYYY-MM-DD'),
-        '14:00',
-        v_id_servicio,
-        v_id_estudiante,
-        v_id_docente,
-        NULL,
-        v_id_psicologo,
-        'ACTIVA'
-    );
-
-EXCEPTION
-    WHEN OTHERS THEN
-        DBMS_OUTPUT.PUT_LINE('PRUEBA CORRECTA: EL TRIGGER IMPIDIO ASIGNAR MAS DE UN TIPO DE PACIENTE ' || SQLERRM);
-END;
-/
-
-
---===============================================
--- 16. PRUEBA FUNCION TIPO PACIENTE SIN PACIENTE
--- DEBE GENERAR ERROR CONTROLADO
---===============================================
-
-DECLARE
-    v_tipo VARCHAR2(50);
-BEGIN
-    v_tipo := fn_tipo_paciente_cita(NULL, NULL, NULL);
-    DBMS_OUTPUT.PUT_LINE('TIPO: ' || v_tipo);
-
-EXCEPTION
-    WHEN OTHERS THEN
-        DBMS_OUTPUT.PUT_LINE('PRUEBA CORRECTA: LA FUNCION IMPIDIO CITA SIN PACIENTE ' || SQLERRM);
-END;
-/
-
-
---===============================================
--- 17. PRUEBA DELETE FISICO PARA TRIGGERS
---===============================================
-
-DECLARE
-    v_id_cita Cita.id_cita%TYPE;
-    v_id_servicio Servicio.id_servicio%TYPE;
-    v_id_psicologo Psicologo.id_psico%TYPE;
-    v_id_admin Administrativo.id_paciente%TYPE;
-BEGIN
-    SELECT id_servicio
-    INTO v_id_servicio
-    FROM Servicio
-    WHERE UPPER(nombre_servicio) = 'ORIENTACION';
-
-    SELECT id_psico
-    INTO v_id_psicologo
-    FROM Psicologo
-    WHERE cedula = 'P-TEST-001';
-
-    SELECT id_paciente
-    INTO v_id_admin
-    FROM Administrativo
-    WHERE cedula = 'A-TEST-001';
-
-    INSERT INTO Cita(
-        id_cita,
-        fecha,
-        hora,
-        id_servicio,
-        id_estudiante,
-        id_docente,
-        id_admin,
-        id_psicologo,
-        estado
-    ) VALUES (
-        seq_id_cita.NEXTVAL,
-        TO_DATE('2026-07-22', 'YYYY-MM-DD'),
-        '15:00',
-        v_id_servicio,
-        NULL,
-        NULL,
-        v_id_admin,
-        v_id_psicologo,
-        'ACTIVA'
-    )
-    RETURNING id_cita INTO v_id_cita;
-
-    DELETE FROM Cita
-    WHERE id_cita = v_id_cita;
-END;
-/
-
-SELECT * 
-FROM auditoria_cita
-ORDER BY aud_id_auditoria;
-
-SELECT *
-FROM estadistica_atenciones
-ORDER BY id_psicologo, id_servicio, tipo_paciente;
-
-
---===============================================
--- 18. PRUEBA TABLA REPORTE_ATENCIONES
--- NOTA: EN EL REPO ACTUAL NO SE ENCONTRO
--- PROCEDIMIENTO sp_generar_reporte_atenciones
---===============================================
-
+-- 10. REPORTE BASICO CON HORA DATE
 INSERT INTO reporte_atenciones(
     id_reporte,
     fecha_generacion,
@@ -632,7 +298,7 @@ SELECT
     TO_DATE('2026-07-31', 'YYYY-MM-DD'),
     c.id_cita,
     c.fecha,
-    TO_DATE(c.hora, 'HH24:MI'),
+    c.hora,
     p.id_psico,
     p.primer_nombre || ' ' || p.apellido_paterno,
     CASE
@@ -640,92 +306,17 @@ SELECT
         WHEN c.id_docente IS NOT NULL THEN c.id_docente
         WHEN c.id_admin IS NOT NULL THEN c.id_admin
     END,
-    fn_nombre_paciente_cita(
-        c.id_estudiante,
-        c.id_docente,
-        c.id_admin
-    ),
-    fn_tipo_paciente_cita(
-        c.id_estudiante,
-        c.id_docente,
-        c.id_admin
-    ),
+    fn_nombre_paciente_cita(c.id_estudiante, c.id_docente, c.id_admin),
+    fn_tipo_paciente_cita(c.id_estudiante, c.id_docente, c.id_admin),
     s.nombre_servicio,
     USER
 FROM Cita c
-INNER JOIN Psicologo p
+JOIN Psicologo p
     ON c.id_psicologo = p.id_psico
-INNER JOIN Servicio s
+JOIN Servicio s
     ON c.id_servicio = s.id_servicio
 WHERE c.fecha BETWEEN TO_DATE('2026-07-01', 'YYYY-MM-DD')
 AND TO_DATE('2026-07-31', 'YYYY-MM-DD')
 AND c.estado = 'ACTIVA';
 
-SELECT *
-FROM reporte_atenciones
-ORDER BY id_reporte;
-
-
---===============================================
--- 19. PRUEBA NEGATIVA REPROGRAMAR CITA INEXISTENTE
---===============================================
-
-BEGIN
-    sp_reprogramar_cita(
-        999999,
-        TO_DATE('2026-08-01', 'YYYY-MM-DD'),
-        '08:00',
-        999999,
-        999999
-    );
-
-EXCEPTION
-    WHEN OTHERS THEN
-        DBMS_OUTPUT.PUT_LINE('PRUEBA CORRECTA: NO SE REPROGRAMO CITA INEXISTENTE ' || SQLERRM);
-END;
-/
-
-
---===============================================
--- 20. PRUEBA NEGATIVA ELIMINAR CITA INEXISTENTE
---===============================================
-
-BEGIN
-    sp_eliminar_cita(999999);
-
-EXCEPTION
-    WHEN OTHERS THEN
-        DBMS_OUTPUT.PUT_LINE('PRUEBA CORRECTA: NO SE ELIMINO CITA INEXISTENTE ' || SQLERRM);
-END;
-/
-
-
---===============================================
--- 21. CONSULTAS FINALES DE VERIFICACION GENERAL
---===============================================
-
-SELECT * FROM cargo;
-SELECT * FROM facultad;
-SELECT * FROM carrera;
-SELECT * FROM servicio;
-
-SELECT * FROM Psicologo;
-SELECT * FROM Estudiante;
-SELECT * FROM Docente;
-SELECT * FROM Administrativo;
-
-SELECT * FROM Tlf_Estudiante;
-SELECT * FROM Tlf_Docente;
-SELECT * FROM Tlf_Admin;
-
-SELECT * FROM Cita
-ORDER BY id_cita;
-
-SELECT * FROM auditoria_cita
-ORDER BY aud_id_auditoria;
-
-SELECT * FROM estadistica_atenciones
-ORDER BY id_psicologo, id_servicio, tipo_paciente;
-
-SELECT * FROM reporte_atenciones
-ORDER BY id_reporte;
+SELECT * FROM reporte_atenciones ORDER BY id_reporte;
